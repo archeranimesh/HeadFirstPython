2b6edd733146a32bc0976cd0d82e548f8de1bb7e
Animesh Bhadra <animeshb@archerimagine.com>
2019-11-09 20:02:00 -0600

    Checked Log in

--- SRC/Chapter_10-Function-Decorators/02_simple_webapp.py
+++ SRC/Chapter_10-Function-Decorators/02_simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 app.secret_key = "YouWillNeverGuessMySecretKey"
@@ -32,16 +33,19 @@ def check_status() -> str:
 
 
 @app.route("/page1")
+@check_logged_in
 def page1() -> str:
     return "this is page 1"
 
 
 @app.route("/page2")
+@check_logged_in
 def page2() -> str:
     return "this is page 2"
 
 
 @app.route("/page3")
+@check_logged_in
 def page3() -> str:
     return "this is page 3"
 
