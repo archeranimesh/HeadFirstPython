935c6bd98611e5f6a69136f547ab776c5f41150a
Animesh Bhadra <animeshb@archerimagine.com>
2019-12-13 20:02:00 -0600

    Chapter 09 | UseDatabase __enter__ created

--- SRC/Chapter_09-The-Context-Management-Protocol/jinja_template/DBcm.py
+++ SRC/Chapter_09-The-Context-Management-Protocol/jinja_template/DBcm.py
@@ -7,8 +7,10 @@ class UseDatabase:
         self.configuration = config
 
     # this method add's the setup code
-    def __enter__(self):
-        pass
+    def __enter__(self) -> "cursor":
+        self.conn = mysql.connector.connect(**self.configuration)
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
     # https://stackoverflow.com/questions/1984325/explaining-pythons-enter-and-exit
     # this method hold's the teardown code.
