bda33126b1ccb9d08937d853e2cb2a8fda896044
Animesh Bhadra <animeshb@archerimagine.com>
2020-03-23 20:02:00 -0600

    Chapter 09 | initial UseDatabase class created

---
+++ SRC/Chapter_09-The-Context-Management-Protocol/jinja_template/DBcm.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+
+class UseDatabase:
+    # this method performs initialization
+    def __init__(self):
+        pass
+
+    # this method add's the setup code
+    def __enter__(self):
+        pass
+
+    # https://stackoverflow.com/questions/1984325/explaining-pythons-enter-and-exit
+    # this method hold's the teardown code.
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
